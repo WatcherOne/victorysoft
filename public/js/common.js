ca0d97868b0b741a853eebd1ea934a53b6fa6848
zhubo <286154864@qq.com>
2018-01-05 17:14:08 +0800

    轮播配置差不多

--- public/js/common.js
+++ public/js/common.js
@@ -1,9 +1,16 @@
 var mySwiper = new Swiper('.swiper-container', {
-	autopaly: 2000,
+	autoplay: 5000,
+	effect: 'coverflow',
+	speed: 500,
 	loop: true,
+	
+	// 禁止鼠标滑动切换
+	noSwiping: true,
 	
 	// 如果需要分页器
 	pagination: '.swiper-pagination',
+	// 可点击分页器
+	paginationClickable: true,
 	
 	// 如果需要前进后退按钮
 	nextButton: '.swiper-button-next',
